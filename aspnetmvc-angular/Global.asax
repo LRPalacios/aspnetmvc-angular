945c449653f90e5c52b8dd4f2c05e1d11451a412
LRPalacios <luisz54@hotmail.com>
2015-07-27 20:54:06 -0600

    start project

---
+++ aspnetmvc-angular/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="aspnetmvc_angular.MvcApplication" Language="C#" %>
---
+++ aspnetmvc-angular/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspnetmvc_angular.Startup))]
+namespace aspnetmvc_angular
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
