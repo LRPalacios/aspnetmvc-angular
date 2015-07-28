9c05ef0d2cc4a28e0b6d18bd1b0bc5268da807a2
LRPalacios <luisz54@hotmail.com>
2015-07-27 22:44:57 -0600

    add configurations

---
+++ aspnetmvc-angular/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aspnetmvc_angular.Models
+{
+    public class Course:BaseEntity
+    {
+        public string Number { get; set; }
+
+        public string Name { get; set; }
+
+        public string Instructor { get; set; }
+    }
+}
---
+++ aspnetmvc-angular/Models/Instructor.cs
@@ -0,0 +1,11 @@
+﻿namespace aspnetmvc_angular.Models
+{
+    public class Instructor:BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int RoomNumber { get; set; }
+    }
+}
